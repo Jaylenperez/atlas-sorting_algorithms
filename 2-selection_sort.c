e80ad031197992db14c9d14e261a4442e67940a3
jaylenperez <jaylenestella24@gmail.com>
2024-04-08 22:05:51 -0500

    fixed syntax error

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
     for (i = 0; i < size - 1; i++)
     {
         min_idx = i;
-        for (j = 1 + 1; j < size; j++)
+        for (j = i + 1; j < size; j++)
         {
             if (array[j] < array[min_idx])
                 min_idx = j;
