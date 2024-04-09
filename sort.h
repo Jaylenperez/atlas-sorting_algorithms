2acbf38437e9379a1a9dc12b76534b6a41e8d3c2
jaylenperez <jaylenestella24@gmail.com>
2024-04-08 22:11:10 -0500

    betty compliance

--- sort.h
+++ sort.h
@@ -6,9 +6,9 @@
 
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
