a90b79a7a7a605fc38d9ed74e2da1f3dcdf28127
jaylenperez <jaylenestella24@gmail.com>
2024-04-08 20:16:15 -0500

    added prototypes to header and cleaned up bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,10 +7,10 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j:
+    size_t i, j;
     int temp;
 
-    for (i = 0; i < size - 1, i++)
+    for (i = 0; i < size - 1; i++)
     {
         for (j = 0; j < size - i - 1; j++)
         {
